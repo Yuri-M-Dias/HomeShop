5d1ed12742a4f65387f0aafed1d5cd38c4f0de5b
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-07-07 13:29:39 -0300

    Adicionada classe base para tratar das requisições com o servidor.

---
+++ app/src/main/java/br/ufg/inf/homeshop/activity/CartActivity.java
@@ -0,0 +1,16 @@
+package br.ufg.inf.homeshop.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.ufg.inf.homeshop.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+
+
+}
--- app/src/main/java/br/ufg/inf/homeshop/activity/MainActivity.java
+++ app/src/main/java/br/ufg/inf/homeshop/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package br.ufg.inf.homeshop;
+package br.ufg.inf.homeshop.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.ufg.inf.homeshop.R;
 
 public class MainActivity extends AppCompatActivity {
 
