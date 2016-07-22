4398fada58f3f7f0975abf4c141e1c8af17bf8c2
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-07-21 21:55:16 -0300

    Acrescentada validação de erro para QRCode inválido

--- app/src/main/java/br/ufg/inf/homeshop/activity/QRCodeActivity.java
+++ app/src/main/java/br/ufg/inf/homeshop/activity/QRCodeActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import br.ufg.inf.homeshop.R;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class QRCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
@@ -41,14 +43,25 @@ public class QRCodeActivity extends AppCompatActivity implements ZXingScannerVie
         String marketId = getMarketId(textoQRCode);
         String productId = getProductId(textoQRCode);
 
-        SharedPreferences settings = this.getSharedPreferences(MainActivity.PREFERENCES_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putLong("marketId", Long.valueOf(marketId));
-        editor.apply();
+        if (areParamsValid(marketId, productId)) {
+            SharedPreferences settings = this.getSharedPreferences(MainActivity.PREFERENCES_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putLong("marketId", Long.valueOf(marketId));
+            editor.apply();
 
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra("marketId", productId);
-        startActivity(intent);
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra("marketId", productId);
+            startActivity(intent);
+        } else {
+            showToast(getString(R.string.qr_code_invalido));
+            finish();
+        }
+    }
+
+    private void showToast(String message) {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, message, duration);
+        toast.show();
     }
 
     private String getMarketId(String textoQRCode) {
@@ -68,5 +81,9 @@ public class QRCodeActivity extends AppCompatActivity implements ZXingScannerVie
             return matcher.group(1);
         }
         return null;
+    }
+
+    private boolean areParamsValid(String marketId, String productId) {
+        return !("".equals(marketId) || "".equals(productId));
     }
 }
